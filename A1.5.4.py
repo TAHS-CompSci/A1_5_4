32788e0e084e7877d64e25e3f078fb09157b6ec5
Duck_With_Knife <57916650+nt3177@users.noreply.github.com>
2020-03-03 13:18:11 -0600

    Update A1.5.4.py

--- A1.5.4.py
+++ A1.5.4.py
@@ -96,11 +96,11 @@ for item in items:
 
   
 #Money Portion
-subtotal = Label(root3,text = 'Subtotal  $'+str(sub))
+subtotal = Label(root3,text = 'Subtotal  $0.00')
 subtotal.grid(row = 1,column = 3)
-tax = Label(root3,text = 'Total Tax  $'+str(taxr))
+tax = Label(root3,text = 'Total Tax  $0.00')
 tax.grid(row = 2,column = 3)
-total = Label(root3,text = 'Total  $'+str(taxr + sub))
+total = Label(root3,text = 'Total  $0.00')
 total.grid(row = 3,column = 3)
 
 
