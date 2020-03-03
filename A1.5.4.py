267eb652bb8f668f0cca532faf633f740b47748e
Duck_With_Knife <57916650+nt3177@users.noreply.github.com>
2020-03-03 13:14:06 -0600

    Update A1.5.4.py

--- A1.5.4.py
+++ A1.5.4.py
@@ -30,6 +30,7 @@ class Substance():
     total.config(text='Total  ${}'.format(totalAm))
 
 
+
 #Functions
 def delete():
   """
@@ -68,16 +69,19 @@ sub = 0.00 # subtotal money
 taxr = 0.04 # tax rate 4 %
 items = [{'Hot Dogs':'2.00','Pretel':'1.00','Nachos':'3.00','Pickle': '1.00','Pizza':'2.00'},{'Pepsi':'1.00','Sprite':'1.00','Root Beer':'1.00'},{'Butterfinger':'1.00','Laffy_Taffy':'0.98','Heath':'1.00'}]
 
-
-#other misc items
-delButt = Button(text = 'Remove Selected', command = delete)
-delButt.grid(row=5,column = 3)
-
+#canvas
+canvas = Canvas(root, height=600, width=600, relief=RAISED, bg='white')
+canvas.grid(row=6, column=0,columnspan=3)
 
 #Receipt
-lb = Listbox(root) 
+root3= tk.Tk()
+lb = Listbox(root3) 
 lb.insert(END,'    -Concession Stand-')
-lb.grid(row = 6,column = 3,columnspan = 2)
+lb.grid(row = 6,column = 4,columnspan = 3)
+
+#other misc items
+delButt = Button(root3,text = 'Remove Selected', command = delete)
+delButt.grid(row=5,column = 4)
 
 #Categories
 for item in items:
@@ -92,16 +96,16 @@ for item in items:
 
   
 #Money Portion
-subtotal = Label(root,text = 'Subtotal  $'+str(sub))
+subtotal = Label(root3,text = 'Subtotal  $'+str(sub))
 subtotal.grid(row = 1,column = 3)
-tax = Label(root,text = 'Total Tax  $'+str(taxr))
+tax = Label(root3,text = 'Total Tax  $'+str(taxr))
 tax.grid(row = 2,column = 3)
-total = Label(root,text = 'Total  $'+str(taxr + sub))
+total = Label(root3,text = 'Total  $'+str(taxr + sub))
 total.grid(row = 3,column = 3)
 
 
 #menu
-menu = Menu(root) 
+menu = Menu(root3) 
 root.config(menu= menu) 
 helpmenu = Menu(menu) 
 menu.add_cascade(label='Help',menu = helpmenu)
