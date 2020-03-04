90fa34d799714442e59edba8c36bd8fc48b16e23
Duck_With_Knife <57916650+nt3177@users.noreply.github.com>
2020-03-04 13:20:39 -0600

    therkelsen
    dfgrdfg

--- A1.5.4.py
+++ A1.5.4.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
-
+import random
 #Classes
 class Substance():
   """
@@ -10,9 +10,10 @@ class Substance():
   def __init__(self,root,r,c,food, pay):
     self.food = food
     self.pay = pay
-    thing = Button(text='{} for ${}'.format(food,pay),command = self.receipt)
-    thing.grid(row = r,column = c)
- 
+    self.thing = Button(text='{} for ${}'.format(food,pay),command = self.receipt, bg='white')
+    self.thing.grid(row = r,column = c)
+    self.photo = tk.PhotoImage(file = food +'.gif')
+
   def receipt(self):
     """
     This Function Does all the math for the receipt, and adds on to the receipt.
@@ -29,7 +30,10 @@ class Substance():
     subtotal.config(text='Subtotal  ${}'.format(sub))
     total.config(text='Total  ${}'.format(totalAm))
 
-
+  def addImage(self):
+    img=canvas.create_image(random.randint(20,600),random.randint(20.600),image=self.photo)
+    shapes.append(img)
+    shapes.append('linebreak')
 
 #Functions
 def delete():
@@ -60,7 +64,7 @@ def about():
 
 
 root = tk.Tk()
-
+root.config(bg='salmon')
 #variables
 categ = ['Food','Bevarage','Candy'] # Categories
 r = 0 # row
@@ -68,9 +72,9 @@ c = 0 # column
 sub = 0.00 # subtotal money
 taxr = 0.04 # tax rate 4 %
 items = [{'Hot Dogs':'2.00','Pretel':'1.00','Nachos':'3.00','Pickle': '1.00','Pizza':'2.00'},{'Pepsi':'1.00','Sprite':'1.00','Root Beer':'1.00'},{'Butterfinger':'1.00','Laffy_Taffy':'0.98','Heath':'1.00'}]
-
+shapes=['linebreak']
 #canvas
-canvas = Canvas(root, height=600, width=600, relief=RAISED, bg='white')
+canvas = Canvas(root, height=600, width=600, relief=RAISED, bg='grey')
 canvas.grid(row=6, column=0,columnspan=3)
 
 #Receipt
@@ -80,13 +84,13 @@ lb.insert(END,'    -Concession Stand-')
 lb.grid(row = 6,column = 4,columnspan = 3)
 
 #other misc items
-delButt = Button(root3,text = 'Remove Selected', command = delete)
+delButt = Button(root3,text = 'Remove Selected', command = delete, bg='red')
 delButt.grid(row=5,column = 4)
 
 #Categories
 for item in items:
   categName = categ[items.index(item)]
-  categLabel = Label(text = categName )
+  categLabel = Label(text = categName, bg='salmon' )
   categLabel.grid(row = r,column = c)
   for key,value in item.items():
     r += 1
