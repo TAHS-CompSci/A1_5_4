4189b15c53184630bd79948b80885149c715a9a5
KB3180 <KB3180@k12.sd.us>
2020-03-04 13:01:32 -0600

    Code

---
+++ Project A1_5_4.py
@@ -0,0 +1,13 @@
+
+
+stock = {'Hot Dogs': 40, 
+ 'Pretzels': 30, 
+ 'Pizza': 50, 
+ 'Nachos': 70, 
+ 'Pickel': 20, 
+ 'Heath': 50, 
+ 'Butterfinger': 35, 
+ 'Laffy_Taffy': 90, 
+ 'Pepsi': 45, 
+ 'Sprite': 65, 
+ 'Root Beer': 85}
