2032638f8b527c2c25007a2a7912082de29d3eda
KB3180 <KB3180@k12.sd.us>
2020-03-05 13:15:43 -0600

    Unknown
    ....

--- Project A1_5_4.py
+++ Project A1_5_4.py
@@ -1,6 +1,6 @@
 
 
-food_stock = {'Hot Dogs': 75, 
+food_stock = {'Hot Dogs': 75,   #This part of code deals with a list of the food items and the stock of those items
  'Salted Pretzels': 80, 
  'Slice Of Pizza': 80, 
  'Nachos': 70, 
@@ -18,10 +18,9 @@ food_stock = {'Hot Dogs': 75,
 'Fritos Orginal': 75,
 'Ruffles': 30,
 'Sun Chips': 40,
-'Hot Cheetos': 60,
+'Hot Cheetos': 60,}
 
-
-for key, value in food_stock.items():
+for key, value in food_stock: #This part of the code is to test to make sure the list works correctly. in which it does when this file was committed.
     print (key)
     print (value)
     
