d52515fb71b135a8a64026addf6a2431ced269c0
KB3180 <KB3180@k12.sd.us>
2020-03-05 13:06:32 -0600

    Food Stock Final

--- Project A1_5_4.py
+++ Project A1_5_4.py
@@ -1,17 +1,27 @@
 
 
-stock = {'Hot Dogs': 40, 
- 'Salted Pretzels': 30, 
- 'Slice Of Pizza': 50, 
+food_stock = {'Hot Dogs': 75, 
+ 'Salted Pretzels': 80, 
+ 'Slice Of Pizza': 80, 
  'Nachos': 70, 
- 'Pickel': 20,
+ 'Pickle': 20,
  'Popcorn': 100,  
  'Lays Chips Onion': 50, 
  'Doritos': 60, 
  'Cool Ranch Doritos': 50, 
- 'Heath': 50, 
- 'Butterfinger': 35, 
- 'Laffy_Taffy': 90, 
- 'Pepsi': 45, 
- 'Sprite': 65, 
- 'Root Beer': 85}
+ 'Pringles (Small can)': 65, 
+ 'Cheetos': 35,  
+'Funions': 20, 
+'Caramel Corn': 25, 
+'Cheddar Popcorn': 50,
+'Fritos Chili': 75,
+'Fritos Orginal': 75,
+'Ruffles': 30,
+'Sun Chips': 40,
+'Hot Cheetos': 60,
+
+
+for key, value in food_stock.items():
+    print (key)
+    print (value)
+    
