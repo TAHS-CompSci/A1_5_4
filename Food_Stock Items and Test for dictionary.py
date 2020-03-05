9e30eb8b5c00b36e3799dd167cf3cae20aac9bd2
KB3180 <KB3180@k12.sd.us>
2020-03-05 13:14:11 -0600

    Food_Stock Dictionary + Test

---
+++ Food_Stock Items and Test for dictionary.py
@@ -0,0 +1,26 @@
+
+
+food_stock = {'Hot Dogs': 75,   #This part of code deals with a list of the food items and the stock of those items
+ 'Salted Pretzels': 80, 
+ 'Slice Of Pizza': 80, 
+ 'Nachos': 70, 
+ 'Pickle': 20,
+ 'Popcorn': 100,  
+ 'Lays Chips Onion': 50, 
+ 'Doritos': 60, 
+ 'Cool Ranch Doritos': 50, 
+ 'Pringles (Small can)': 65, 
+ 'Cheetos': 35,  
+'Funions': 20, 
+'Caramel Corn': 25, 
+'Cheddar Popcorn': 50,
+'Fritos Chili': 75,
+'Fritos Orginal': 75,
+'Ruffles': 30,
+'Sun Chips': 40,
+'Hot Cheetos': 60,}
+
+for key, value in food_stock: #This part of the code is to test to make sure the list works correctly. in which it does when this file was committed.
+    print (key)
+    print (value)
+    
