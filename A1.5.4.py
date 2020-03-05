ebf778c231a22a8909edbf621ccde2f69ee7b73a
ae3155 <57917046+ae3155@users.noreply.github.com>
2020-03-05 13:11:11 -0600

    updated

--- A1.5.4.py
+++ A1.5.4.py
@@ -67,9 +67,9 @@ r = 0 # row
 c = 0 # column
 sub = 0.00 # subtotal money
 taxr = 0.06 # tax rate 6 %
-items = [{'Hot Dogs':'2.00','Pretel':'1.00','Nachos':'3.00','Pickle': '1.00','Pizza':'2.00'},
-{'Pepsi':'1.00','Sprite':'1.00','Root Beer':'1.00', 'Pepsi':'1.00','Sprite':'1.00', 'Root Bear':'1.00', 'Mtn. Dew':'1.00', 'Dr. pepper.':'1.00', 'Extra pibb':'2.00', 'Coca Cola.':'1.00', 'Fanta Orange':'2.00', 'Mellow Yellow':'2.00', 'Crush':'1.00', '7-Up':'1.00', 'Pepsi Wild Cherry':'2.00', 'Diet Coke':'1.50', 'Diet mtn dew':'1.50', 'Diet pepsi':'1.50', '7 up cherry':'2.00', 'A&W cream soda':'1.75', 'Sun Drop':'2.00', 'Squirt':'2.00', 'Jones Cane Sugar Soda - Blue Rhasberry':'3.00','Jones Cane Sugar Soda - Cherry':'3.00', 'Fanta Grape':'2.00', 'Crush Strawberry':'2.00', 'Fanta Pineapple':'2.00', 'Water':'0.75', 'Lemonade':'1.30'},
-{'Butterfinger':'1.00','Laffy_Taffy':'0.98','Heath':'1.00'}]
+items = [{'Hot Dogs':'2.00','Pretel':'1.00','Nachos':'3.00','Pickle': '1.00','Pizza':'2.00'}]
+Soda=[{'Pepsi':'1.00','Sprite':'1.00','Root Beer':'1.00', 'Pepsi':'1.00','Sprite':'1.00', 'Root Bear':'1.00', 'Mtn. Dew':'1.00', 'Dr. pepper.':'1.00', 'Extra pibb':'2.00', 'Coca Cola.':'1.00', 'Fanta Orange':'2.00', 'Mellow Yellow':'2.00', 'Crush':'1.00', '7-Up':'1.00', 'Pepsi Wild Cherry':'2.00', 'Diet Coke':'1.50', 'Diet mtn dew':'1.50', 'Diet pepsi':'1.50', '7 up cherry':'2.00', 'A&W cream soda':'1.75', 'Sun Drop':'2.00', 'Squirt':'2.00', 'Jones Cane Sugar Soda - Blue Rhasberry':'3.00','Jones Cane Sugar Soda - Cherry':'3.00', 'Fanta Grape':'2.00', 'Crush Strawberry':'2.00', 'Fanta Pineapple':'2.00', 'Water':'0.75', 'Lemonade':'1.30'}]
+Candy=[{'Butterfinger':'1.00','Laffy_Taffy':'0.98','Heath':'1.00'}]
 
 #canvas
 canvas = Canvas(root, height=600, width=600, relief=RAISED, bg='white')
