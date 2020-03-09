cd067e409687041a76d552b3266b91d2e085db28
NK3216 <57916851+NK3216@users.noreply.github.com>
2020-03-09 12:59:32 -0500

    work

---
+++ ICS-Myamoto.py
@@ -0,0 +1,16 @@
+"""
+1.5.4 design a gui
+Myamoto
+ice cream shop gui
+"""
+
+
+
+from tkinter import * 
+
+root = Tk()
+root.geometry('300x300')
+
+c = Canvas(root, height=250, width=300)
+
+root.mainloop()
