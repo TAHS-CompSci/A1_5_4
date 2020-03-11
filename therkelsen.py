107aac49f2b52e639e7c263c26e28ae24b001e9c
Drayzdin <57916828+Dt3202@users.noreply.github.com>
2020-03-11 13:08:24 -0500

    Update therkelsen.py

--- therkelsen.py
+++ therkelsen.py
@@ -6,21 +6,29 @@ c = 0 # column
 
 root = tk.Tk()
 root.config(bg= 'light blue')
-canvas = Canvas(root, height = 300, width = 600, relief = RAISED, bg = 'gray',bd = 13)
+canvas = Canvas(root, height =600, width = 600, relief = RAISED, bg = 'gray',bd = 13)
 canvas.grid(row = 2, column = 0, columnspan = 3 )
 
 
-def poo():
-    print ('have a good day you little shit')
+def poo(flavor):
+    global coords
+    circles = []
+    circles.append(canvas.create_oval(coords, fill=flavor))
+    coords[1] -= 30
+    coords[3] -= 30
 
-coco = Button(root, relief=RIDGE,bg='light gray', text='coco', command = poo)
+
+coords = [250, 450, 350, 550]
+coco = Button(root, relief=RIDGE,bg='light gray', text='coco', command = lambda: poo('brown'))
 coco.grid(row=0,column=0)
 
-straw= Button(root, relief=RIDGE,bg='light gray', text='strawberry', command = poo)
+straw= Button(root, relief=RIDGE,bg='light gray', text='strawberry', command = lambda: poo('pink'))
 straw.grid(row=0,column=1)
 
-van = Button(root, relief=RIDGE,bg='light gray', text='vanilla', command = poo)
+van = Button(root, relief=RIDGE,bg='light gray', text='vanilla', command = lambda: poo('white'))
 van.grid(row=0,column=2)
 items ={'coco': '1.00', 'vanilla': '1.25', 'strawberry': '1.50'}
+
+
 
 root.mainloop()
