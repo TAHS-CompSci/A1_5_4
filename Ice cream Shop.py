adcc0dfc7bd5b4b0538bde7dd86659995b42296d
KB3180 <KB3180@k12.sd.us>
2020-03-11 13:19:37 -0500

    Ice Cream Shop
    Tried to figure out the Receipt

--- Ice cream Shop.py
+++ Ice cream Shop.py
@@ -19,3 +19,51 @@ for i in range(4):
     radio[i].grid(row=i,column=2)
 data.set(3)
 root.mainloop()
+
+Ice_Cream_Sizes_Prices = {'Single': 1.50, 'Double': 2.50, 'Triple': 3.50}
+print (Ice_Cream_Sizes_Prices)  
+
+  global taxr
+        global sub
+        lb.insert(END,'{}      ${}'.format(self.Ice_Cream,self.pay))
+        lb.insert(END,'-'*30) 
+        lb.see(END)
+        sub = round(float(sub)+ float(self.pay),2)
+        taxAm = round(float(sub)*float(taxr),2)
+        totalAm=round(float(sub)+float(taxAm),2)
+        tax.config(text='Total Tax  ${}'.format(taxAm))
+        subtotal.config(text='Subtotal  ${}'.format(sub))
+        total.config(text='Total  ${}'.format(totalAm))
+
+        def delete():
+   
+    global sub
+    global taxr
+    item = lb.curselection()
+    Ice_Cream = lb.get(ACTIVE)
+    Ice_cream = Ice_Cream[0:len(Ice_Cream)-11]
+    canvas.delete(shapes[item[0]])
+    if lb.index(item) % 2 == 1:
+    lb.delete(item)
+    lb.insert(item,'Removed')
+    for item in items.keys():
+    for x,y in items[item].items():
+    if x.title() == food:
+    sub = round(float(sub)-float(y),2)
+    taxAm= round(float(sub)*float(taxr),2)
+    totalAm = round(float(sub)+float(taxAm),2)
+    tax.config(text='Total Tax  ${}'.format(taxAm))
+    subtotal.config(text='Subtotal  ${}'.format(sub))
+    total.config(text='Total  ${}'.format(totalAm))
+
+
+root = tk.Tk(className='Ice Cream Stand')
+root.config(bg='Ice Cream')
+
+r = 0 
+c = 0 
+sub = 0.00 
+taxr = 0.75
+items = {'Ice Cream'{'Single':1.50, 'Double': 2.50, 'Triple':3.50}
+
+        
